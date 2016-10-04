a48d6b381bd1ff9c70fcd78b80c9b42f501333f3
sutt94 <sutt94@gmail.com>
2016-10-04 17:16:21 -0400

    test commit

---
+++ ElderSourceApp/Controllers/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElderSourceApp.Controllers
+{
+    public class Home : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
