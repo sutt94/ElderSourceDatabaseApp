1339fd955ee8e528b8db9d312ea031c46cbd906d
sutt94 <sutt94@gmail.com>
2016-10-12 17:02:34 -0400

    Basic Login Working

---
+++ ElderSourceApp/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using ElderSourceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElderSourceApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(UserModel model)
+        {
+            ElderSourceEntities le = new ElderSourceEntities();
+            var s = le.GetLoginInfo(model.UserName, model.Password);
+
+            var item = s.FirstOrDefault();
+            if (item == "Success")
+            {
+
+                return View("UserLandingView");
+            }
+            else if (item == "User Does not Exists")
+
+            {
+                ViewBag.NotValidUser = item;
+
+            }
+            else
+            {
+                ViewBag.Failedcount = item;
+            }
+
+            return View("Index");
+        }
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ElderSourceApp/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElderSourceApp.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
