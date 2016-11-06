ff298e2d215ed4a1e69c0a7d87ed26dae4534a0a
willmcross <hatchinmorenos@hotmail.com>
2016-11-06 01:01:06 -0400

    Added Report models

---
+++ ElderSourceApp/Models/ReportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElderSourceApp.Models
+{
+    public class ReportModel
+    {
+
+        public int reportId { get; set; }
+
+        public string reportName { get; set; }
+
+        public DateTime dateCreated { get; set; }
+
+        public DateTime lastDatePaid { get; set; }
+
+    }
+}
---
+++ ElderSourceApp/Models/ReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElderSourceApp.Models
+{
+    public class ReportViewModel
+
+    {
+        public string ReportName { get; set; }
+
+        public DateTime timeCreated { get; set; }
+        public DateTime filterStart { get; set; }
+
+        public DateTime filterEnd { get; set; }
+    }
+}
